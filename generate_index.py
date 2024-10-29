81ce4e3d5d1dfb770888d7c860da5d6b212490b5
F. Y. <8799996+fyuniv@users.noreply.github.com>
2024-10-29 09:10:25 -0400

    use separated css

--- generate_index.py
+++ generate_index.py
@@ -11,18 +11,22 @@ def generate_index_html(directory):
     # Generate HTML content
     html_content = "<html><head><title>Math Seminars at The Graduate Center of CUNY</title></head><body>"
     html_content += '<h1 style="text-align: center;">Weekly Bulletin of Math Seminars at The Graduate Center of CUNY</h1>'
-    html_content += "<ul>"
     
-    for i, file in enumerate(html_files):
-        file_title = os.path.splitext(file)[0] 
-        file_path = os.path.join(directory, file)
-        mod_time = datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%Y-%m-%d %H:%M:%S')
-        if i == 0:
-            html_content += f"<li><strong><a href='{file}'>{file}</a> - {mod_time}</strong></li>"
-        else:
-            html_content += f"<li><a href='{file}'>Week {file_title}</a> - {mod_time}</li>"
+    # Add Recent section
+    html_content += "<h2>Recent</h2><ul>"
+    for i, file in enumerate(html_files[:4]):
+        file_title = os.path.splitext(file)[0]
+        html_content += f"<li><a href='{file}'>Week {file_title}</a></li>"
+    html_content += "</ul>"
     
-    html_content += "</ul></body></html>"
+    # Add Past section
+    html_content += "<h2>Past</h2><ul>"
+    for file in html_files[4:]:
+        file_title = os.path.splitext(file)[0]
+        html_content += f"<li><a href='{file}'>Week {file_title}</a></li>"
+    html_content += "</ul>"
+    
+    html_content += "</body></html>"
     
     # Write the index.html file
     with open(os.path.join(directory, 'index.html'), 'w') as f:
